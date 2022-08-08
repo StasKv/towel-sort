525b18ba16e7e3c7fa24aaaba806a6b576a16874
Your Name <you@example.com>
2022-08-09 00:10:15 +0300

    feat: add task decision

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix == undefined) {
+        return [];
+    }
+    if (matrix.length == 0 || matrix == undefined) {
+        return [];
+    }
+    matrix.forEach((element, index) => {
+        if (index % 2 != 0) {
+            element = element.reverse();
+        }
+    });
+    let fromStrToArr = matrix.join().split(",");
+    let ArrOfNumb = fromStrToArr.map(Number);
+
+    return ArrOfNumb;
+};
